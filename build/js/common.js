18c1dfdb8a801aff47f23455312f7ee0b1e0f3d7
HOME_PC\Максим <maximkirigetov@gmail.com>
2017-07-15 16:12:00 +0300

    first-com

---
+++ build/js/common.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+	// hero slider
+	$('.js-hero-slider').slick({
+		dots: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 700,
+		fade: true,
+		arrows: true,
+		prevArrow: $('.slider-arrow_left'),
+		nextArrow: $('.slider-arrow_right')
+	});
+
+	// product slider
+	$('.js-product-slider').slick({
+		dots: true,
+		infinite: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		arrows: true,
+		prevArrow: $('.slider-arrow_left'),
+		nextArrow: $('.slider-arrow_right')
+	});
+
+
+});
